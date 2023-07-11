b09cd9baf3950c92574efb17f4a14c1f43a28d3f
Ben Turner <ext.ben.turner@nomadrail.com>
2023-07-11 15:44:59 +0200

    update of checkstyle.xml location

--- check-code.sh
+++ check-code.sh
@@ -12,10 +12,10 @@ cd ..
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo $changed_java_files
 
-if [ ! -f lib/checkstyle.xml ]; then
-  echo "Please ensure lib/checkstyle.xml exists!"
+if [ ! -f checkstyle.xml ]; then
+  echo "Please ensure checkstyle.xml exists!"
   exit 1
 fi
 
-java -jar .cache/checkstyle-10.12.1-all.jar -c lib/checkstyle.xml $changed_java_files
+java -jar .cache/checkstyle-10.12.1-all.jar -c checkstyle.xml $changed_java_files
 
